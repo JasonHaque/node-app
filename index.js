9651a415d3d73cdbca72f615cd0e90d47422b183
jason <zahinjason220434@gmail.com>
2019-07-20 14:02:09 +0600

    OS module

--- index.js
+++ index.js
@@ -8,3 +8,14 @@ const path = require('path');
 var pathObj = path.parse(__filename);
 console.log(pathObj);
 
+//using node.js os module
+
+const os = require('os');
+
+var totalmem = os.totalmem();
+var freemem = os.freemem();
+
+//using ES6 template strings
+console.log(`Total memory: ${totalmem}`);
+console.log(`Free memory: ${freemem}`);
+
