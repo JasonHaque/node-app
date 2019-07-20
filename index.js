051c5ad667ec9d9e90505394aff7f68a2f198b68
jason <zahinjason220434@gmail.com>
2019-07-20 13:52:35 +0600

    using path module

--- index.js
+++ index.js
@@ -1,3 +1,10 @@
-var logger = require('./logger');
+const log = require('./logger');
+log('message');
 
-logger.log('message');
+
+//using node.js path module
+const path = require('path');
+
+var pathObj = path.parse(__filename);
+console.log(pathObj);
+
